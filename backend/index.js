d37520a68762f59b03ef38167de7c684ab4ed40f
antknees <47506456+antknees@users.noreply.github.com>
2024-02-04 21:58:25 -0800

    fix nth check and mongoos

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const helmet = required('helmet');
+const helmet = require('helmet');
 
 const app = express();
 const port = 8000;
