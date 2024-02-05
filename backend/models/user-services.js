550b9072d79188ea2d27b034e5eed44c3a64357d
antknees <47506456+antknees@users.noreply.github.com>
2024-02-04 22:02:18 -0800

    Fix sql injection?

--- backend/models/user-services.js
+++ backend/models/user-services.js
@@ -82,7 +82,11 @@ async function validateUser(cred){
 
 async function findUserByName(name) {
   console.log(name);
-  return await userModel.find({ user: name });
+  return await userModel.findAll({
+    where: {
+      username: name
+    }
+  });
 }
 
 
